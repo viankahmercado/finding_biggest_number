61e96cedfc623651bc44f3dfd0be53ec223872b4
viankahmercado <angelviankahmercado@gmail.com>
2024-01-12 22:33:26 +0800

    get the inputter numbers

--- finding_biggest_number.py
+++ finding_biggest_number.py
@@ -10,19 +10,33 @@ number_1 = float(input("Enter the first number: "))
 number_2 = float(input("Enter the second number: "))
 number_3 = float(input("Enter the third number: "))
 
-# find the biggest number among the three (algorithm)
-if number_1 >= number_2 >= number_3:
-    biggest = number_1
-elif number_1 >= number_3 >= number_2:
-    biggest = number_1
-elif number_2 >= number_1 >= number_3:
-    biggest = number_2
-elif number_2 >= number_3 >= number_1:
-    biggest = number_2
-elif number_3 >= number_1 >= number_2:
-    biggest = number_3
-else:
-    biggest = number_3
+# find the biggest number among the three using if-else (algorithm)
+def find_biggest_number(number_1, number_2, number_3):
+    if number_1 >= number_2 >= number_3:
+        largest = number_1
+    elif number_1 >= number_3 >= number_2:
+        largest = number_1
+    elif number_2 >= number_1 >= number_3:
+        largest = number_2
+    elif number_2 >= number_3 >= number_1:
+        largest = number_2
+    elif number_3 >= number_1 >= number_2:
+        largest = number_3
+    else:
+        largest = number_3
+
+    return largest
+
+# get user input for three numbers
+def calculate_biggest():
+     try:
+        first_number = get_inputted_number("Enter the first number:")
+        second_number = get_inputted_number("Enter the second number:")
+        third_number = get_inputted_number("Enter the third number:")
+    except ValueError as e:
+        # Display an error message for invalid input
+        messagebox.showerror("Error", str(e))
+        return
 
 # create the first window
 app = tk.Tk()
