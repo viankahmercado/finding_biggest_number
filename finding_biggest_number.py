8170201dd0735b4351f31877c67ae2cdd0bac85b
viankahmercado <angelviankahmercado@gmail.com>
2024-01-12 20:58:50 +0800

    storing variables and input numbers is successful

--- finding_biggest_number.py
+++ finding_biggest_number.py
@@ -3,6 +3,9 @@
 
 # pseudocode
 # ask user to input three numbers
+number_1 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the second number: "))
+number_3 = float(input("Enter the third number: "))
 
 # find the biggest number among the three (algorithm)
 
