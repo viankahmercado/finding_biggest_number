356535e444b2ce100d563df20bb7f915df432a84
viankahmercado <angelviankahmercado@gmail.com>
2024-01-12 20:48:30 +0800

    pseudocode

--- finding_biggest_number.py
+++ finding_biggest_number.py
@@ -1,3 +1,9 @@
 # Assignment 4
 # Ask user to input 3 numbers. Find and print the biggest number using only if-else statement 
 
+# pseudocode
+# ask user to input three numbers
+
+# find the biggest number among the three (algorithm)
+
+# print the result
