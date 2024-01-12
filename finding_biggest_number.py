ce6589ed80dfeb9954f4003837124f9b693ec2b8
viankahmercado <angelviankahmercado@gmail.com>
2024-01-12 21:12:17 +0800

    algorithm using boolean relational operator and if-else

--- finding_biggest_number.py
+++ finding_biggest_number.py
@@ -8,5 +8,17 @@ number_2 = float(input("Enter the second number: "))
 number_3 = float(input("Enter the third number: "))
 
 # find the biggest number among the three (algorithm)
-
+if number_1 >= number_2 >= number_3:
+    largest = number_1
+elif number_1 >= number_3 >= number_2:
+    largest = number_1
+elif number_2 >= number_1 >= number_3:
+    largest = number_2
+elif number_2 >= number_3 >= number_1:
+    largest = number_2
+elif number_3 >= number_1 >= number_2:
+    largest = number_3
+else:
+    largest = number_3
+    
 # print the result
